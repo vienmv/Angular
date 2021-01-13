a4d0291c981ee49b7da2f3ca4f58bcd499f0ed0a
VienMV <vienmv.dev@gmail.com>
2021-01-13 11:23:52 +0700

    Component 1123 130121

---
+++ EShopping/src/app/cart-sumary/cart-sumary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-sumary',
+  templateUrl: './cart-sumary.component.html',
+
+})
+export class CartSumaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
