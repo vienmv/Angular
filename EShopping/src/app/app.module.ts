345a8531316271565837f7d12a34ad12a50003fc
VienMV <vienmv.dev@gmail.com>
2021-01-13 11:23:20 +0700

    Component 1123 130121

--- EShopping/src/app/app.module.ts
+++ EShopping/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CartHeaderComponent } from './cart-header/cart-header.component';
+import { CartSumaryComponent } from './cart-sumary/cart-sumary.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { PromoCodeComponent } from './promo-code/promo-code.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartHeaderComponent,
+    CartSumaryComponent,
+    ProductListComponent,
+    PromoCodeComponent
   ],
   imports: [
     BrowserModule
